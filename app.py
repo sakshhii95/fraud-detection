55580614471f5b8eb66a8f1f801b6cd4cc6d3476
Your Name <sakshhii95@gmail.com>
2026-04-09 23:39:28 +0530

    update

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import numpy as np
 import joblib
 
 # Load saved system
-data = joblib.load("./fraud_full_system.joblib")
+data = joblib.load("fraud_full_system.joblib")
 
 pipeline = data["pipeline"]
 features = data["features"]
-encoders = data["encoders"]   # ✅ ADD THIS
+encoders = data["encoders"]   
 
 # Page setup
 st.set_page_config(page_title="Fraud Detection", layout="centered")
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Fraud Detection", layout="centered")
 st.title("💳 Credit Card Fraud Detection")
 st.write("Enter transaction details and click Predict")
 
-# ✅ ONLY 4 INPUTS (clean UI)
+
 
 # Amount
 amount = st.number_input("Amount", value=0.0)
@@ -56,7 +56,7 @@ if st.button("Predict"):
     prediction = pipeline.predict(input_df)[0]
     probability = pipeline.predict_proba(input_df)[0][1]
 
-    if prediction >=0.11:
+    if prediction >=0.1:
         st.error(f"🚨 Fraud Detected!\nProbability: {probability:.2f}")
     else:
         st.success(f"✅ Legit Transaction\nProbability: {probability:.2f}")
